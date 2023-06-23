06df3d4b3f82ab038baddb3bc3d66e41dc8bd21b
brittany <brittanyfreitas9@gmail.com>
2023-06-23 10:38:32 -0400

    fix form

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,66 +1,111 @@
-//this page will include the booking information again as well as the calnder widget
-import {useLayoutEffect } from "react";
-import BookingFooter from "./BookingFooter.js"
+import React, { useState } from "react";
+import BookingFooter from "./BookingFooter.js";
 
 const Bookings = () => {
+  const [name, setName] = useState("");
+  const [pronouns, setPronouns] = useState("");
+  const [number, setNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [isNameVisible, setIsNameVisible] = useState(true);
+  const [isPronounsVisible, setIsPronounsVisible] = useState(true);
+  const [isNumberVisible, setIsNumberVisible] = useState(true);
+  const [isEmailVisible, setIsEmailVisible] = useState(true);
+  const [isMessageVisible, setIsMessageVisible] = useState(true);
 
-  useLayoutEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+  const handleInputChange = (event, setState, setIsVisible) => {
+    const { value } = event.target;
+    setState(value);
+    setIsVisible(value === "");
+  };
 
   return (
     <>
-    <section className="book">
-      <h2>Contact Us</h2>
-      <div className="sectionContent wrapper">
-      <div className="getInTouch">
-     <h3>Ready to get started? Send us an email with any questions you may have. </h3>
-  
-      </div>
+      <section className="book">
+        <h2>Contact Us</h2>
+        <div className="sectionContent wrapper">
+          <div className="getInTouch">
+            <h3>
+              Ready to get started? Send us an email with any questions you may have.
+            </h3>
+          </div>
 
-      <div className="formContainer">
-      <h4>Get in touch</h4>
-      <p>Getting started with therapy is rarely easy. We're happy to answer any questions you may have or chat to see if our approach is a good fit for your unique needs and goals. Send us an email below:</p>
-      <form 
-        action="https://formsubmit.co/cassandra@therapyintheclouds.ca"
-        method="POST"
-      >
-        <label for="_captcha"></label>
-        <input type="hidden" name="_captcha" value="false" />
+          <div className="formContainer">
+            <h4>Get in touch</h4>
+            <p>
+              Getting started with therapy is rarely easy. We're happy to answer any questions you may have or chat to
+              see if our approach is a good fit for your unique needs and goals. Send us an email below:
+            </p>
+            <form action="https://formsubmit.co/cassandra@therapyintheclouds.ca" method="POST">
+              <label htmlFor="_captcha"></label>
+              <input type="hidden" name="_captcha" value="false" />
 
-        <div class="formItem">
-          <label for="name">Name</label>
-          <input className="name" type="text" id="name" name="name" required />
+              <div className="formItem">
+                {isNameVisible && <label htmlFor="name">Name</label>}
+                <input
+                  className="name"
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={name}
+                  onChange={(event) => handleInputChange(event, setName, setIsNameVisible)}
+                  required
+                />
+              </div>
+
+              <div className="formItem">
+                {isPronounsVisible && <label htmlFor="pronouns">Pronouns</label>}
+                <input
+                  className="pronouns"
+                  type="text"
+                  id="pronouns"
+                  name="pronouns"
+                  value={pronouns}
+                  onChange={(event) => handleInputChange(event, setPronouns, setIsPronounsVisible)}
+                />
+              </div>
+
+              <div className="formItem">
+                {isNumberVisible && <label htmlFor="number">Phone Number</label>}
+                <input
+                  className="number"
+                  type="tel"
+                  id="number"
+                  name="number"
+                  value={number}
+                  onChange={(event) => handleInputChange(event, setNumber, setIsNumberVisible)}
+                />
+              </div>
+
+              <div className="formItem">
+                {isEmailVisible && <label htmlFor="email">Email</label>}
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={(event) => handleInputChange(event, setEmail, setIsEmailVisible)}
+                  required
+                />
+              </div>
+
+              <div className="formItem">
+                {isMessageVisible && <label htmlFor="message">Message</label>}
+                <textarea
+                  name="message"
+                  id="message"
+                  cols="20"
+                  rows="10"
+                  value={message}
+                  onChange={(event) => handleInputChange(event, setMessage, setIsMessageVisible)}
+                ></textarea>
+              </div>
+              <button type="submit">Send</button>
+            </form>
+          </div>
         </div>
-
-        <div class="formItem">
-          <label for="pronouns">Pronouns</label>
-          <input class="proouns" type="text" id="pronouns" name="pronouns" />
-        </div>
-
-        <div class="formItem">
-          <label for="number">Phone Number</label>
-          <input class="number" type="tel" id="number" name="number" />
-        </div>
-
-        <div class="formItem">
-          <label for="email">Email</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-
-        <div class="formItem">
-          <label for="message">Message</label>
-          <textarea name="message" id="message" cols="20" rows="10"></textarea>
-        </div>
-        <button type="submit">Send</button>
-      </form>
-        
-        
-      </div>
-      </div>
-     
-    </section>
-    <BookingFooter/>
+      </section>
+      <BookingFooter />
     </>
   );
 };
